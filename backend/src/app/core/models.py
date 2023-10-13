2773fb27b02b55b82d472ebb1b980c447e398595
nicamanas <xyukimiku@gmail.com>
2023-10-13 17:35:47 +0800

    Created RoleApplications model

--- backend/src/app/core/models.py
+++ backend/src/app/core/models.py
@@ -9,13 +9,16 @@ from app.core.database import Base
 
 # Enums and Constants
 
+
 class Status(enum.Enum):
     ACTIVE = "active"
     INACTIVE = "inactive"
 
+
 class SkillStatus(enum.Enum):
     ACTIVE = "active"
     INACTIVE = "inactive"
+
 
 class StaffSysRole(enum.Enum):
     STAFF = "staff"
@@ -23,18 +26,22 @@ class StaffSysRole(enum.Enum):
     MANAGER = "manager"
     INACTIVE = "inactive"
 
+
 class RoleStatus(enum.Enum):
     ACTIVE = "active"
     INACTIVE = "inactive"
+
 
 class StaffSkillsStatus(enum.Enum):
     ACTIVE = "active"
     UNVERIFIED = "unverified"
     IN_PROGRESS = "in-progress"
 
+
 class RoleType(enum.Enum):
     PRIMARY = "primary"
     SECONDARY = "secondary"
+
 
 class StaffRoleStatus(enum.Enum):
     ACTIVE = "active"
@@ -44,6 +51,7 @@ class StaffRoleStatus(enum.Enum):
 
 # LMS Models
 
+
 class StaffDetails(Base):
     __tablename__ = "staff_details"
 
@@ -51,21 +59,30 @@ class StaffDetails(Base):
     fname = Column(String(50))
     lname = Column(String(50))
     dept = Column(String(50))
-    email = Column(String(50))  
+    email = Column(String(50))
     phone = Column(String(20))
     biz_address = Column(String(255))
-    sys_role = Column(Enum(*[e.value for e in StaffSysRole]), default=StaffSysRole.STAFF.value)
+    sys_role = Column(Enum(*[e.value for e in StaffSysRole]),
+                      default=StaffSysRole.STAFF.value)
 
-    staff_reporting_officer_staff = relationship("StaffReportingOfficer", back_populates="staff", primaryjoin='and_(StaffDetails.staff_id == StaffReportingOfficer.staff_id)')
-    staff_reporting_officer_officer = relationship("StaffReportingOfficer", back_populates="reporting_officer", primaryjoin='and_(StaffDetails.staff_id == StaffReportingOfficer.RO_id)')
+    staff_reporting_officer_staff = relationship(
+        "StaffReportingOfficer", back_populates="staff", primaryjoin='and_(StaffDetails.staff_id == StaffReportingOfficer.staff_id)')
+    staff_reporting_officer_officer = relationship(
+        "StaffReportingOfficer", back_populates="reporting_officer", primaryjoin='and_(StaffDetails.staff_id == StaffReportingOfficer.RO_id)')
 
     staff_skills_staff = relationship("StaffSkills", back_populates="staff")
 
     staff_roles_staff = relationship("StaffRoles", back_populates="staff")
 
-    role_listings_source = relationship("RoleListings", back_populates="staff_details_source", primaryjoin='and_(StaffDetails.staff_id == RoleListings.role_listing_source)')
-    role_listings_creator = relationship("RoleListings", back_populates="staff_details_creator", primaryjoin='and_(StaffDetails.staff_id == RoleListings.role_listing_creator)')
-    role_listings_updater = relationship("RoleListings", back_populates="staff_details_updater", primaryjoin='and_(StaffDetails.staff_id == RoleListings.role_listing_updater)')
+    role_listings_source = relationship("RoleListings", back_populates="staff_details_source",
+                                        primaryjoin='and_(StaffDetails.staff_id == RoleListings.role_listing_source)')
+    role_listings_creator = relationship("RoleListings", back_populates="staff_details_creator",
+                                         primaryjoin='and_(StaffDetails.staff_id == RoleListings.role_listing_creator)')
+    role_listings_updater = relationship("RoleListings", back_populates="staff_details_updater",
+                                         primaryjoin='and_(StaffDetails.staff_id == RoleListings.role_listing_updater)')
+    
+    staff_details_source = relationship("RoleApplications", back_populates="staff_details_source",
+                                        primaryjoin='and_(RoleApplications.staff_id == StaffDetails.staff_id)')
 
     def __init__(self, staff_id, fname, lname, dept, email, phone, biz_address, sys_role):
         self.staff_id = staff_id
@@ -77,14 +94,19 @@ class StaffDetails(Base):
         self.biz_address = biz_address
         self.sys_role = sys_role
 
+
 class StaffReportingOfficer(Base):
     __tablename__ = "staff_reporting_officer"
 
-    staff_id = Column(Integer, ForeignKey('staff_details.staff_id'), primary_key=True)
-    RO_id = Column(Integer, ForeignKey('staff_details.staff_id'), primary_key=True)
+    staff_id = Column(Integer, ForeignKey(
+        'staff_details.staff_id'), primary_key=True)
+    RO_id = Column(Integer, ForeignKey(
+        'staff_details.staff_id'), primary_key=True)
 
-    staff = relationship("StaffDetails", back_populates="staff_reporting_officer_staff", foreign_keys=[staff_id])
-    reporting_officer = relationship("StaffDetails", back_populates="staff_reporting_officer_officer", foreign_keys=[RO_id])
+    staff = relationship(
+        "StaffDetails", back_populates="staff_reporting_officer_staff", foreign_keys=[staff_id])
+    reporting_officer = relationship(
+        "StaffDetails", back_populates="staff_reporting_officer_officer", foreign_keys=[RO_id])
 
     def __init__(self, staff_id, reporting_officer_id):
         self.staff_id = staff_id
@@ -92,12 +114,14 @@ class StaffReportingOfficer(Base):
 
 # LJPS Models
 
+
 class SkillDetails(Base):
     __tablename__ = "skill_details"
 
     skill_id = Column(Integer, primary_key=True)
     skill_name = Column(String(50))
-    skill_status = Column(Enum(*[e.value for e in SkillStatus]), default=SkillStatus.ACTIVE.value)
+    skill_status = Column(
+        Enum(*[e.value for e in SkillStatus]), default=SkillStatus.ACTIVE.value)
 
     staff_skills_skill = relationship("StaffSkills", back_populates="skill")
     role_skills_skill = relationship("RoleSkills", back_populates="skill")
@@ -107,19 +131,22 @@ class SkillDetails(Base):
         self.skill_name = skill_name
         self.skill_status = skill_status
 
+
 class RoleDetails(Base):
     __tablename__ = "role_details"
 
     role_id = Column(Integer, primary_key=True)
     role_name = Column(String(50))
     role_description = Column(String(5000))
-    role_status = Column(Enum(*[e.value for e in RoleStatus]), default=RoleStatus.ACTIVE.value)
-    
+    role_status = Column(
+        Enum(*[e.value for e in RoleStatus]), default=RoleStatus.ACTIVE.value)
+
     role_skills_role = relationship("RoleSkills", back_populates="role")
 
     staff_roles_role = relationship("StaffRoles", back_populates="role")
 
-    role_listings_role_id = relationship("RoleListings", back_populates="role_role_id", primaryjoin='and_(RoleDetails.role_id == RoleListings.role_id)')
+    role_listings_role_id = relationship(
+        "RoleListings", back_populates="role_role_id", primaryjoin='and_(RoleDetails.role_id == RoleListings.role_id)')
 
     def __init__(self, role_id, role_name, role_description, role_status):
         self.role_id = role_id
@@ -127,27 +154,33 @@ class RoleDetails(Base):
         self.role_description = role_description
         self.role_status = role_status
 
+
 class StaffSkills(Base):
     __tablename__ = "staff_skills"
 
-    staff_id = Column(Integer, ForeignKey('staff_details.staff_id'), primary_key=True)
-    skill_id = Column(Integer, ForeignKey('skill_details.skill_id'), primary_key=True)
-    ss_status = Column(Enum(*[e.value for e in StaffSkillsStatus]), default=StaffSkillsStatus.ACTIVE.value)
+    staff_id = Column(Integer, ForeignKey(
+        'staff_details.staff_id'), primary_key=True)
+    skill_id = Column(Integer, ForeignKey(
+        'skill_details.skill_id'), primary_key=True)
+    ss_status = Column(Enum(
+        *[e.value for e in StaffSkillsStatus]), default=StaffSkillsStatus.ACTIVE.value)
 
     staff = relationship("StaffDetails", back_populates="staff_skills_staff")
     skill = relationship("SkillDetails", back_populates="staff_skills_skill")
-
 
     def __init__(self, staff_id, skill_id, ss_status):
         self.staff_id = staff_id
         self.skill_id = skill_id
         self.ss_status = ss_status
 
+
 class RoleSkills(Base):
     __tablename__ = "role_skills"
 
-    role_id = Column(Integer, ForeignKey('role_details.role_id'), primary_key=True)
-    skill_id = Column(Integer, ForeignKey('skill_details.skill_id'), primary_key=True)
+    role_id = Column(Integer, ForeignKey(
+        'role_details.role_id'), primary_key=True)
+    skill_id = Column(Integer, ForeignKey(
+        'skill_details.skill_id'), primary_key=True)
 
     role = relationship("RoleDetails", back_populates="role_skills_role")
     skill = relationship("SkillDetails", back_populates="role_skills_skill")
@@ -156,13 +189,18 @@ class RoleSkills(Base):
         self.role_id = role_id
         self.skill_id = skill_id
 
+
 class StaffRoles(Base):
     __tablename__ = "staff_roles"
 
-    staff_id = Column(Integer, ForeignKey('staff_details.staff_id'), primary_key=True)
-    staff_role = Column(Integer, ForeignKey('role_details.role_id'), primary_key=True)
-    role_type = Column(Enum(*[e.value for e in RoleType]), default=RoleType.PRIMARY.value)
-    sr_status = Column(Enum(*[e.value for e in StaffRoleStatus]), default=StaffRoleStatus.ACTIVE.value)
+    staff_id = Column(Integer, ForeignKey(
+        'staff_details.staff_id'), primary_key=True)
+    staff_role = Column(Integer, ForeignKey(
+        'role_details.role_id'), primary_key=True)
+    role_type = Column(Enum(*[e.value for e in RoleType]),
+                       default=RoleType.PRIMARY.value)
+    sr_status = Column(
+        Enum(*[e.value for e in StaffRoleStatus]), default=StaffRoleStatus.ACTIVE.value)
 
     staff = relationship("StaffDetails", back_populates="staff_roles_staff")
     role = relationship("RoleDetails", back_populates="staff_roles_role")
@@ -175,6 +213,7 @@ class StaffRoles(Base):
 
 # SBRP Models
 
+
 class RoleListings(Base):
     __tablename__ = "role_listings"
 
@@ -183,25 +222,39 @@ class RoleListings(Base):
     role_listing_desc = Column(String(5000))
     role_listing_source = Column(Integer, ForeignKey('staff_details.staff_id'))
     role_listing_open = Column(DateTime, default=func.now())
-    role_listing_close = Column(DateTime, default=datetime.utcnow() + timedelta(weeks=2))
+    role_listing_close = Column(
+        DateTime, default=datetime.utcnow() + timedelta(weeks=2))
     # system generated fields
-    role_listing_creator = Column(Integer, ForeignKey('staff_details.staff_id'))
-    role_listing_ts_create = Column(DateTime(timezone=True), default=func.now())
-    role_listing_updater = Column(Integer, ForeignKey('staff_details.staff_id'))
-    #update time automatically
-    role_listing_ts_update = Column(DateTime(timezone=True), onupdate=func.now())
+    role_listing_creator = Column(
+        Integer, ForeignKey('staff_details.staff_id'))
+    role_listing_ts_create = Column(
+        DateTime(timezone=True), default=func.now())
+    role_listing_updater = Column(
+        Integer, ForeignKey('staff_details.staff_id'))
+    # update time automatically
+    role_listing_ts_update = Column(
+        DateTime(timezone=True), onupdate=func.now())
 
-    role_role_id = relationship("RoleDetails", back_populates="role_listings_role_id")
-    staff_details_source = relationship("StaffDetails", back_populates="role_listings_source", primaryjoin='and_(RoleListings.role_listing_source == StaffDetails.staff_id)')
-    staff_details_creator = relationship("StaffDetails", back_populates="role_listings_creator", primaryjoin='and_(RoleListings.role_listing_creator == StaffDetails.staff_id)')
-    staff_details_updater = relationship("StaffDetails", back_populates="role_listings_updater", primaryjoin='and_(RoleListings.role_listing_updater == StaffDetails.staff_id)')
+    role_role_id = relationship(
+        "RoleDetails", back_populates="role_listings_role_id")
+    staff_details_source = relationship("StaffDetails", back_populates="role_listings_source",
+                                        primaryjoin='and_(RoleListings.role_listing_source == StaffDetails.staff_id)')
+    staff_details_creator = relationship("StaffDetails", back_populates="role_listings_creator",
+                                         primaryjoin='and_(RoleListings.role_listing_creator == StaffDetails.staff_id)')
+    staff_details_updater = relationship("StaffDetails", back_populates="role_listings_updater",
+                                         primaryjoin='and_(RoleListings.role_listing_updater == StaffDetails.staff_id)')
+    
+    role_app_listing_source = relationship("RoleApplications", back_populates="role_app_listing_source",
+                                       primaryjoin='and_(RoleListings.role_listing_id == RoleApplications.role_listing_id)')
+
 
 class SkillDetailsSBRP(Base):
     __tablename__ = "skill_details_sbrp"
 
     skill_id = Column(Integer, primary_key=True)
     skill_name = Column(String(50))
-    skill_status = Column(Enum(*[e.value for e in SkillStatus]), default=SkillStatus.ACTIVE.value)
+    skill_status = Column(
+        Enum(*[e.value for e in SkillStatus]), default=SkillStatus.ACTIVE.value)
 
     def __init__(self, skill_id, skill_name, skill_status):
         self.skill_id = skill_id
@@ -209,7 +262,18 @@ class SkillDetailsSBRP(Base):
         self.skill_status = skill_status
 
 
+class RoleApplications(Base):
+    __tablename__ = "role_applications"
 
-    
+    role_app_id = Column(Integer, primary_key=True, autoincrement=False)
+    role_listing_id = Column(Integer, ForeignKey(
+        'role_listings.role_listing_id'))
+    staff_id = Column(Integer, ForeignKey('staff_details.staff_id'))
+    role_app_status = Column(
+        Enum(*[e.value for e in Status]), default=Status.ACTIVE.value)
+    role_app_ts_create = Column(DateTime(timezone=True), default=func.now())
 
-    
+    role_app_listing_source = relationship("RoleListings", back_populates="role_app_listing_source",
+                                       primaryjoin='and_(RoleApplications.role_listing_id == RoleListings.role_listing_id)')
+    staff_details_source = relationship("StaffDetails", back_populates="staff_details_source",
+                                        primaryjoin='and_(RoleApplications.staff_id == StaffDetails.staff_id)')
