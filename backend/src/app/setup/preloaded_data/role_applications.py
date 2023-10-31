43af6c5a90e6c959dc0d1017c1810d9322831749
Thaddeus <thaddeusleezx@gmail.com>
2023-10-31 23:24:09 +0800

    removed role applications that were causing errors (bug)

--- backend/src/app/setup/preloaded_data/role_applications.py
+++ backend/src/app/setup/preloaded_data/role_applications.py
@@ -5,8 +5,13 @@ DEFAULT_ROLE_APPLICATIONS = [
         "role_app_status": "active"
     }, 
     {
-        "role_listing_id": 1,
+        "role_listing_id": 2,
         "staff_id": 4,
+        "role_app_status": "active"
+    },
+    {
+        "role_listing_id": 3,
+        "staff_id": 5,
         "role_app_status": "active"
     },
 ]
