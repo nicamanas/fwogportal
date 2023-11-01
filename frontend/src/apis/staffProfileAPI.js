e8bbedd11808c2d54bc7ec0a250905aaa745a9d5
nicamanas <xyukimiku@gmail.com>
2023-11-01 16:56:08 +0800

    added endpoint
    endpoint to retrieve all staff profiles

--- frontend/src/apis/staffProfileAPI.js
+++ frontend/src/apis/staffProfileAPI.js
@@ -1,48 +1,63 @@
 export const StaffProfileAPI = {
-  get: async function (id) {
-    try {
-      const response = await fetch(
-        `http://localhost:8003/staff_profile/${id}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+	get: async function (id) {
+		try {
+			const response = await fetch(
+				`http://localhost:8003/staff_profile/${id}`,
+				{
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			)
 
-      if (!response.ok) {
-        console.error("Error fetching staff profile:", response.status);
-      } else {
-        const responseData = await response.json();
-        return responseData;
-      }
-    } catch (error) {
-      console.error("Error fetching staff profile:", error);
-    }
-  },
-  deleteSkill: async function (staff_id, skill_id) {
-    try {
-      const response = await fetch(
-        `http://localhost:8003/staff_profile/${staff_id}/skill/${skill_id}`,
-        {
-          method: "DELETE",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+			if (!response.ok) {
+				console.error("Error fetching staff profile:", response.status)
+			} else {
+				const responseData = await response.json()
+				return responseData
+			}
+		} catch (error) {
+			console.error("Error fetching staff profile:", error)
+		}
+	},
+	getAll: async function () {
+		const response = await fetch("http://localhost:8003/staff_profile", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
 
-      if (!response.ok) {
-        console.error("Error deleting skill profile:", response.status);
-        throw new Error("Error deleting skill profile");
-      } else {
-        console.log("Successfully deleted skill profile");
-        return true;
-      }
-    } catch (error) {
-      console.error("Error deleting skill profile:", error);
-      throw new Error("Error deleting skill profile");
-    }
-  },
-};
+		if (!response.ok) {
+			console.error("Error fetching staff profiles:", response.status)
+		} else {
+			const responseData = await response.json()
+			return responseData
+		}
+	},
+	deleteSkill: async function (staff_id, skill_id) {
+		try {
+			const response = await fetch(
+				`http://localhost:8003/staff_profile/${staff_id}/skill/${skill_id}`,
+				{
+					method: "DELETE",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			)
+
+			if (!response.ok) {
+				console.error("Error deleting skill profile:", response.status)
+				throw new Error("Error deleting skill profile")
+			} else {
+				console.log("Successfully deleted skill profile")
+				return true
+			}
+		} catch (error) {
+			console.error("Error deleting skill profile:", error)
+			throw new Error("Error deleting skill profile")
+		}
+	},
+}
